2712166893f027f2f0a2fd1451b5861a378d24aa
Alin402 <balanescu.alin03@gmail.com>
2024-05-11 23:11:23 +0300

    5

--- Messages/AppMessages.py
+++ Messages/AppMessages.py
@@ -6,6 +6,10 @@ async def receive_messages(websocket, appData, event, lock):
     with lock:
         print("receiving messages...")
         while event.is_set():
-            message = await websocket.recv()
-            print("Received message: ", message["MessageType"])
-            await asyncio.sleep(2)
+            try:
+                message = await websocket.recv()
+                print("Received message: ", message.json()["MessageType"])
+                await asyncio.sleep(2)
+            except Exception as e:
+                print(e)
+                continue
