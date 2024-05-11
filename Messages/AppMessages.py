8fae50adebef121a44bb995bfe67ba2c2af38b20
Alin402 <balanescu.alin03@gmail.com>
2024-05-11 23:49:48 +0300

    1

--- Messages/AppMessages.py
+++ Messages/AppMessages.py
@@ -1,9 +1,18 @@
 import time
 import asyncio
+import json
 
 
-async def receive_messages(websocket, appData, event, lock):
+async def receive_messages(websocket, appData, event, lock, restart):
     with lock:
+        print("receiving messages...")
         while event.is_set():
-            print("receiving messages...")
-            await asyncio.sleep(2)
+            try:
+                message = await websocket.recv()
+                jsonMessage = json.loads(message)
+                messageType = jsonMessage["MessageType"]
+                if messageType is 8:
+                    print("Restarting...")
+            except Exception as e:
+                print(e)
+                continue
