96df30e16ac4b84729bbc628010299af5a0c15e8
Alin402 <balanescu.alin03@gmail.com>
2024-05-12 17:26:18 +0300

    1

--- Wifi/ConnectToWifi.py
+++ Wifi/ConnectToWifi.py
@@ -1,23 +1,14 @@
-import subprocess
+import wifi
+from wifi import Cell, Scheme
 
 
 def connect_to_wifi(ssid, password):
-    try:
-        # Execute the iwlist command to scan for available networks
-        result = subprocess.run(['iwlist', 'wlan0', 'scan'], capture_output=True, text=True)
-
-        # Parse the output to extract SSIDs
-        networks = []
-        current_ssid = None
-        for line in result.stdout.split('\n'):
-            if 'ESSID:' in line:
-                current_ssid = line.split('"')[1]
-            elif 'Quality=' in line and current_ssid:
-                networks.append((current_ssid, line.split('Quality=')[1].split()[0]))
-                current_ssid = None
-
-        for network in networks:
-            print("SSID:", network[0], "Signal:", network[1])
-    except Exception as e:
-        print("Error:", e)
-        return []
+    print("Connecting to wifi... " + ssid + " with password " + password)
+    cells = wifi.Cell.all('wlan0')
+    for cell in cells:
+        if cell.ssid == ssid:
+            scheme = Scheme.for_cell('wlan0', ssid, cell, password)
+            scheme.save()
+            scheme.activate()
+            print("Connected to Wi-Fi network:", ssid)
+            return
