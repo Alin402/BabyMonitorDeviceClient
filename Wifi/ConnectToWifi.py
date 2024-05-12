f76c242b986afcdcab47b6e9bebc8d36389f623a
Alin402 <balanescu.alin03@gmail.com>
2024-05-12 17:27:53 +0300

    2

--- Wifi/ConnectToWifi.py
+++ Wifi/ConnectToWifi.py
@@ -1,14 +1,28 @@
-import wifi
-from wifi import Cell, Scheme
+import time
+import pywifi
+from pywifi import const
 
 
 def connect_to_wifi(ssid, password):
-    print("Connecting to wifi... " + ssid + " with password " + password)
-    cells = wifi.Cell.all('wlan0')
-    for cell in cells:
-        if cell.ssid == ssid:
-            scheme = Scheme.for_cell('wlan0', ssid, cell, password)
-            scheme.save()
-            scheme.activate()
-            print("Connected to Wi-Fi network:", ssid)
-            return
+    wifi = pywifi.PyWiFi()
+    iface = wifi.interfaces()[0]  # Assuming only one wireless interface exists
+
+    iface.disconnect()  # Disconnect from any currently connected network
+
+    profile = pywifi.Profile()
+    profile.ssid = ssid
+    profile.auth = const.AUTH_ALG_OPEN
+    profile.akm.append(const.AKM_TYPE_WPA2PSK)
+    profile.cipher = const.CIPHER_TYPE_CCMP
+    profile.key = password
+
+    iface.remove_all_network_profiles()  # Remove existing profiles
+    tmp_profile = iface.add_network_profile(profile)
+
+    iface.connect(tmp_profile)
+    time.sleep(5)  # Wait for connection to establish
+
+    if iface.status() == const.IFACE_CONNECTED:
+        print("Connected to Wi-Fi network:", ssid)
+    else:
+        print("Failed to connect to Wi-Fi network")
