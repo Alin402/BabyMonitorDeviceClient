efc8bb24b14141cebd1e3212ab2491cf7b574f16
Alin402 <balanescu.alin03@gmail.com>
2024-05-12 16:38:40 +0300

    1

--- Wifi/ConnectToWifi.py
+++ Wifi/ConnectToWifi.py
@@ -1,33 +1,31 @@
 import os
+import subprocess
 
 
-def connect_to_wifi(ssid, password):
+def configure_and_connect_wifi(ssid, password):
+    config_lines = [
+        'country=US',
+        'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev',
+        'update_config=1',
+        '\n',
+        'network={',
+        '\tssid="{}"'.format(ssid),
+        '\tpsk="{}"'.format(password),
+        '}'
+    ]
+    config = '\n'.join(config_lines)
+
     try:
-        config_lines = [
-            'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev',
-            'update_config=1',
-            'country=US',
-            '\n',
-            'network={',
-            '\tssid="{}"'.format(ssid),
-            '\tpsk="{}"'.format(password),
-            '}'
-        ]
-        config = '\n'.join(config_lines)
-
-        # Give access and writing permissions. May have to do this manually beforehand.
-        os.popen("sudo chmod a+w /etc/wpa_supplicant/wpa_supplicant.conf")
-
-        # Write to the configuration file.
-        with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w") as wifi:
+        # Write the configuration to a temporary file
+        with open("/etc/wpa_supplicant/wpa_temp.conf", "w") as wifi:
             wifi.write(config)
 
-        print("Wi-Fi configuration added. Refreshing Wi-Fi configurations...")
-        # Refresh Wi-Fi configurations.
-        os.popen("sudo wpa_cli -i wlan0 reconfigure")
+        # Move the temporary file to the correct location
+        subprocess.run(["sudo", "mv", "/etc/wpa_supplicant/wpa_temp.conf", "/etc/wpa_supplicant/wpa_supplicant.conf"])
 
-        # Connect to the configured Wi-Fi network.
-        print("Connecting to Wi-Fi network:", ssid)
-        os.popen("sudo wpa_cli -i wlan0 reassociate")
+        # Restart the wpa_supplicant service to apply the changes
+        subprocess.run(["sudo", "systemctl", "restart", "wpa_supplicant"])
+
+        print("Wi-Fi configuration added. Connected to Wi-Fi network:", ssid)
     except Exception as e:
-        print("Exception in connect to wifi " + str(e))
+        print("Error:", e)
