77659179d19f5812d5aab248052ac75101e4ce0c
Alin402 <balanescu.alin03@gmail.com>
2024-05-12 17:03:09 +0300

    1

--- Wifi/ConnectToWifi.py
+++ Wifi/ConnectToWifi.py
@@ -3,12 +3,18 @@ from wifi import Cell, Scheme
 
 
 def connect_to_wifi(ssid, password):
-    print("Connecting to wifi... " + ssid + " with password " + password)
+    # Scan for available Wi-Fi networks
     cells = wifi.Cell.all('wlan0')
+
+    # Check if the specified Wi-Fi network is found
     for cell in cells:
         if cell.ssid == ssid:
+            print(cell.ssid)
+            # Connect to the specified Wi-Fi network
             scheme = Scheme.for_cell('wlan0', ssid, cell, password)
             scheme.save()
             scheme.activate()
             print("Connected to Wi-Fi network:", ssid)
             return
+
+    print("Wi-Fi network not found:", ssid)
