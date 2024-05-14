aeaf86c7edf90fe84e65f899c57b74103ca858d3
Alin402 <balanescu.alin03@gmail.com>
2024-05-15 00:26:55 +0300

    1

--- Livestream/StartLivestream.py
+++ Livestream/StartLivestream.py
@@ -5,7 +5,7 @@ import asyncio
 async def start_live_stream(event, appData):
     try:
         print("Starting Livestream...")
-        streamCommand = "libcamera-vid -t 0 --width 640 --height 362  --inline -o - | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://rtmp.livepeer.com/live/8da5-10go-ds2a-e2qd"
+        streamCommand = "libcamera-vid -t 0 --width 640 --height 362  --inline -o - | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv " + appData.StreamingChannelUrl
         print(streamCommand)
         process = await asyncio.create_subprocess_shell(
             streamCommand,
